2ddb87f1b1d3629dafdd16f82878a4cb8a4acc7d
David Sudia <dsudia@gmail.com>
2016-04-01 13:41:08 -0600

    added migration info to create posts

--- reddit-clone/migrations/20160401133325_create_posts_table.js
+++ reddit-clone/migrations/20160401133325_create_posts_table.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('posts', function(table) {
+    table.increments();
+    table.string('title');
+    table.dateTime('date');
+    table.text('description');
+    table.string('image');
+    table.string('author');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('posts');
 };

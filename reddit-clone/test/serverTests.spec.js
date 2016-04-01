a6bf6ba3208bdfea04f139f1e8346037ee6e3154
David Sudia <dsudia@gmail.com>
2016-04-01 14:04:21 -0600

    added test files, db query tests written

---
+++ reddit-clone/test/serverTests.spec.js
@@ -0,0 +1,69 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../src/server/app');
+var knex = require('../db/knex');
+
+var should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('DB routes', function() {
+
+    beforeEach(function(done) {
+        knex.migrate.rollback().then(function() {
+            knex.migrate.latest()
+            .then(function() {
+                return knex.seed.run().then(function() {
+                    done();
+                });
+            });
+        });
+    });
+
+    afterEach(function(done) {
+        knex.migrate.rollback().then(function() {
+            done();
+        });
+    });
+
+    describe('Get all posts', function() {
+
+        it('should get all posts', function(done) {
+            chai.request(server)
+            .get('/getData/posts')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('array');
+                res.body.length.should.equal(3);
+                res.body[0].should.have.property('title');
+                res.body[0].name.should.equal('Aspens in Fall');
+                res.body[0].should.have.property('author');
+                res.body[0].channel.should.equal('Dave');
+                done();
+            });
+        });
+
+    });
+
+    describe('Get all comments', function() {
+
+        it('should get all comments', function(done) {
+            chai.request(server)
+            .get('/getData/comments')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('array');
+                res.body.length.should.equal(3);
+                res.body[0].should.have.property('comm_author');
+                res.body[0].name.should.equal('Mike');
+                res.body[0].should.have.property('post_id');
+                res.body[0].channel.should.equal(1);
+                done();
+            });
+        });
+
+    });
+
+});

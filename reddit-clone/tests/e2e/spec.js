403c1f035a45a1133b9156efe89bbc2b03b7db8b
David Sudia <dsudia@gmail.com>
2016-04-01 13:45:58 -0600

    deleted redundant tests folder

--- reddit-clone/tests/e2e/spec.js
+++
@@ -1,8 +0,0 @@
-describe('myController', function () {
-
-  it('the dom initially has a greeting', function () {
-    browser.get('http://localhost:8888/#/one');
-    expect(element(by.id('greeting')).getText()).toEqual('Hello, World!');
-  });
-
-});

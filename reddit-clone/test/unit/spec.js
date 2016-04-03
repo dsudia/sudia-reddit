1c6500f9940951f884482f531cae93f4c2e39fbe
David Sudia <dsudia@gmail.com>
2016-04-02 21:06:01 -0600

    added list of tests to write

--- reddit-clone/test/unit/spec.js
+++ reddit-clone/test/unit/spec.js
@@ -1,11 +1,8 @@
-describe('test showForm', function () {
-
+describe('test showForm controller', function () {
   var controller = null;
   $scope = null;
 
   beforeEach(module('redditClone'));
-
-
   it('initially has a status of true for hidden items', inject(function ($controller) {
     var scope = {};
     var showForm = $controller('showForm', {
@@ -15,7 +12,7 @@ describe('test showForm', function () {
     scope.hideStatus.should.equal(true);
   }));
 
-  it('initially has a status of true for hidden items', inject(function ($controller) {
+  it('changes value of hideStatus on button click', inject(function ($controller) {
     var scope = {};
     var showForm = $controller('showForm', {
       $scope: scope
@@ -25,6 +22,24 @@ describe('test showForm', function () {
     scope.hideAndShow();
     scope.hideStatus.should.equal(false);
   }));
+});
 
+describe('test addPost', function() {
+
+});
+
+describe('test showComment', function() {
+
+});
+
+describe('test addComment', function() {
+
+});
+
+describe('test change post score', function() {
+
+});
+
+describe('test displayPosts', function() {
 
 });

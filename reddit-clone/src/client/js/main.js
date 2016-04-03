41f66dd30fcaca20f66747ae28ec85dcc6602d97
David Sudia <dsudia@gmail.com>
2016-04-03 16:41:39 -0600

    make add function for posts

--- reddit-clone/src/client/js/main.js
+++ reddit-clone/src/client/js/main.js
@@ -1,28 +1,36 @@
 var app = angular.module('redditClone', []);
 
 // get posts from database
-app.factory('getPosts', function($http) {
+app.factory('dbCalls', function($http) {
   var postClass = {};
   postClass.get = function() {
     return $http.get("/getData/posts");
   };
+  postClass.add = function() {
+    return $http.post('/addData/posts');
+  };
   return postClass;
 });
 
+
 // Show and hide new post form
-app.controller('showForm', function($scope, getPosts) {
+app.controller('formControl', function($scope) {
   $scope.formHideStatus = true;
   $scope.hideAndShowForm = function() {
     $scope.formHideStatus = !$scope.formHideStatus;
   };
 
-  getPosts.get()
-  .then(function(results) {
-    $scope.posts = results.data;
-    console.log($scope.posts);
-  });
+
 });
 
+
+//show posts on page
+app.controller('postControl', function($scope, dbCalls) {
+  dbCalls.get()
+  .then(function(results) {
+    $scope.posts = results.data;
+  });
+});
 
 
 

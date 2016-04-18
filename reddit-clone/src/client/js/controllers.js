fdbfd6851d4a44a184227ab06a0dfbad5bb0e035
David Sudia <dsudia@gmail.com>
2016-04-17 19:32:54 -0600

    added login and register controllers and services

--- reddit-clone/src/client/js/controllers.js
+++ reddit-clone/src/client/js/controllers.js
@@ -91,5 +91,38 @@ app.controller('postControl', function($scope, dbCalls) {
     post.upvote --;
     dbCalls.changeVote(post.id, post.upvote);
   };
-
 });
+
+app.controller('registerController', ['$scope', '$location', 'authService',
+ function($scope, $location, authService) {
+  $scope.user = {};
+  $scope.register = function() {
+    authService.register($scope.user)
+      .then(function(user) {
+        authService.setUserInfo(user);
+        $location.path('/');
+      })
+      .catch(function(err) {
+        // check status code, send appropriate message
+        console.log(err);
+      });
+  };
+}]);
+
+
+app.controller('loginController', ['$rootScope', '$scope', '$location', 'authService',
+  function($rootScope, $scope, $location, authService) {
+  $scope.user = {};
+  $scope.login = function() {
+    authService.login($scope.user)
+      .then(function(user) {
+        authService.setUserInfo(user);
+        $location.path('/');
+        $rootScope.currentUser = authService.getUserInfo();
+      })
+      .catch(function(err) {
+        // check status code, send appropriate message
+        console.log(err);
+      });
+  };
+}]);
--- reddit-clone/src/client/js/services.js
+++ reddit-clone/src/client/js/services.js
@@ -34,3 +34,41 @@ app.service('dbCalls', function($http) {
 
   return postClass;
 });
+
+app.service('authService', ['$http', '$window', function($http, $window) {
+  var user = {};
+  return {
+    login: function(user) {
+      return $http.post('/auth/login', user);
+    },
+    logout: function(user) {
+      user = null;
+      $window.localStorage.clear();
+    },
+    register: function(user) {
+      return $http.post('/auth/register', user);
+    },
+    setUserInfo: function(userData) {
+      $window.localStorage.setItem('user', JSON.stringify(userData.data.data.user));
+      $window.localStorage.setItem('token', JSON.stringify(userData.data.data.token));
+    },
+    getUserInfo: function(userData) {
+      return $window.localStorage.getItem('user');
+    },
+  };
+}]);
+
+app.service('authInterceptor', ['$window', function($window) {
+  return {
+    request: function(config) {
+      // check for token in headers
+      // config.headers['X-requested-with'] = XMLHttpRequest;
+      var token = $window.localStorage.getItem('token');
+      if(token) {
+        config.headers.Authorization = "Bearer " + token;
+        // return $q.resolve(config);
+      }
+      return config;
+    }
+  };
+}]);
